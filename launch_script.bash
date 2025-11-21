7c3511c5fd3ff0b7b9f6a5c51a6ae4cc5a4bdb13
Astrid Geels <moerastrid@gmail.com>
2025-11-21 18:44:12 +0100

    Makefile in git ignore, bash script netjes

--- launch_script.bash
+++ launch_script.bash
@@ -1,13 +1,4 @@
 find * -name "*.java" > sources.txt
 javac @sources.txt
 
-# andere dingen:
-# rm ./**/*.class -> verwijdert alle compiled files
-# java Simulator ./resources/scenario.txt -> run de simulator
-
-# vanuit root makkelijk opstarten:
-# javac -d out @sources.txt
-# java -cp out Simulator resources/scenario.txt
-
-
-javac -d target @sources.txt && java -cp target ajav/Simulator resources/scenario.txt
+#javac -d target @sources.txt && java -cp target ajav/Simulator resources/scenario.txt
